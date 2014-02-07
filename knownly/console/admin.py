eadea74bcadf7a63beb130ba0e752766a1ed7812
Dwight Gunning <dwight@dwightgunning.com>
2014-02-07 13:47:53 +0100

    Improved admin list views with useful columns and filters

--- knownly/console/admin.py
+++ knownly/console/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from knownly.console.models import DropboxUser, DropboxSite
 
-admin.site.register(DropboxUser)
-admin.site.register(DropboxSite)
+class DropboxUserAdmin(ModelAdmin):
+    list_display = ('display_name', 'email', 'subscription_active')
+
+class DropboxSiteAdmin(ModelAdmin):
+    list_display = ('domain', 'dropbox_user')
+    list_filter = ('dropbox_user',)
+
+
+admin.site.register(DropboxUser, DropboxUserAdmin)
+admin.site.register(DropboxSite, DropboxSiteAdmin)
