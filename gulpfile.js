f205268ca2b0c9b8209d554fef650d096dae465d
Dwight Gunning <dwight@dwightgunning.com>
2015-06-14 14:24:09 +0200

    concat javascript dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,35 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass') 
     notify = require('gulp-notify') 
     bower = require('gulp-bower');
+    concat = require('gulp-concat');
 
 var config = {
-     sassPath: './static/sass',
+     sassPath: './static/sass/',
      bowerDir: './bower_components' 
 }
 
 gulp.task('bower', function() { 
-    return bower()
-         .pipe(gulp.dest(config.bowerDir)) 
+  return bower().pipe(gulp.dest(config.bowerDir)) 
 });
 
 gulp.task('icons', function() { 
     return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
-        .pipe(gulp.dest('static/fonts')); 
+      .pipe(gulp.dest('static/fonts')); 
 });
+
+gulp.task('js', function() {
+    return gulp.src([
+        config.bowerDir + '/jquery/dist/jquery.min.js',
+        config.bowerDir + '/bootstrap-sass-official/assets/javascripts/bootstrap.min.js',
+      ])
+      .pipe(concat('app.js'))
+      .pipe(gulp.dest('static/js/'));  
+})
 
 gulp.task('css', function() { 
     return sass(config.sassPath, {
              style: 'compressed',
              loadPath: [
-                 './resources/sass',
                  config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
                  config.bowerDir + '/fontawesome/scss',
              ]
@@ -39,4 +47,4 @@ gulp.task('css', function() { 
      gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
 });
 
-  gulp.task('default', ['bower', 'icons', 'css']);
+  gulp.task('default', ['bower', 'icons', 'css', 'js']);
