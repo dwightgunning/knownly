5ca486a862f36e5b2709e5841396c7265a973977
Dwight Gunning <dwight@dwightgunning.com>
2015-07-01 00:47:15 +0200

    Fab file for production deployment

---
+++ fabfile.py
@@ -0,0 +1,38 @@
+from fabric.api import *
+
+prod_server = '178.79.169.241'
+
+def base_env():
+    env.user = 'knownly'
+    env.root = '/sites/knownly'
+    env.directory = '%s/source' % env.root
+    env.activate = 'source %s/venvs/current/bin/activate' % env.root
+    env.supervisor_webapp_program = 'knownly'
+
+def prod():
+    base_env()    
+    env.hosts = [prod_server]
+
+def run_under_venv(command):
+    run(env.activate + ' && ' + command)
+
+def git_pull(remote='origin', branch='prod'):
+    """Updates the repository."""
+    with cd(env.directory):
+        run('git fetch --all')
+        run('git reset --hard %s/%s' % (remote, branch))
+
+def restart_website():
+    run("sudo /usr/local/bin/supervisorctl restart %s" % env.supervisor_webapp_program)
+
+def deploy(remote='origin', branch='master'):
+    """Run the actual deployment steps, e.g: $ fab prod deploy"""
+    with cd(env.directory):
+        git_pull(remote=remote, branch=branch)
+        run_under_venv("pip install -r %s/requirements.txt" % env.directory)
+        run_under_venv("python manage.py syncdb --noinput")
+        run_under_venv("python manage.py migrate --noinput")
+        run('gulp') # Build the statics
+        run_under_venv("python manage.py collectstatic --noinput")
+    
+    restart_website()
