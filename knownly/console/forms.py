133a6a3c3f6d4aa68f1954718b85dcc614a9d848
Dwight Gunning <dwight@dwightgunning.com>
2015-11-06 14:36:03 +0100

    Fix Website creation form
    The incorrect indentation meant the custom clean function for the domain field
    was not being called. Nor the custom save function.

--- knownly/console/forms.py
+++ knownly/console/forms.py
@@ -1,9 +1,11 @@
+import logging
 import re
 
 from django import forms
 
 from knownly.console.models import DropboxSite
 
+logger = logging.getLogger(__name__)
 
 class WebsiteForm(forms.ModelForm):
 
@@ -13,31 +15,31 @@ class WebsiteForm(forms.ModelForm):
         domain = forms.CharField(
             error_messages={'required': 'Please enter a domain.'})
 
-        def clean_domain(self):
-            domain = self.cleaned_data['domain']
+    def clean_domain(self):
+        domain = self.cleaned_data['domain']
 
-            if domain.endswith("knownly.com"):
-                domain = domain[:-3] + "net"
+        if domain.endswith("knownly.com"):
+            domain = self.cleaned_data['domain'] = domain[:-3] + "net"
 
-            if DropboxSite.objects.filter(domain__iexact=domain).exists():
-                raise forms.ValidationError('This domain has already been '
-                                            'claimed. Please choose another.')
+        if DropboxSite.objects.filter(domain__iexact=domain).exists():
+            raise forms.ValidationError('This domain has already been '
+                                        'claimed. Please choose another.')
 
-            valid_domains = re.compile(r'^[a-zA-Z\d-]{,63}'
-                                       '(\.[a-zA-Z\d-]{,63})'
-                                       '(\.[a-zA-Z\d-]{,63})?.$')
-            if not valid_domains.match(domain):
-                raise forms.ValidationError('This domain is invalid. '
-                                            'Please try another.')
+        valid_domains = re.compile(r'^[a-zA-Z\d-]{,63}'
+                                   '(\.[a-zA-Z\d-]{,63})'
+                                   '(\.[a-zA-Z\d-]{,63})?.$')
+        if not valid_domains.match(domain):
+            raise forms.ValidationError('This domain is invalid. '
+                                        'Please try another.')
 
-            if domain == 'knownly.net':
-                raise forms.ValidationError("Sorry, that one's ours.")
+        if domain == 'knownly.net':
+            raise forms.ValidationError("Sorry, that one's ours.")
 
-            return domain
+        return domain
 
-        def save(self, commit=True):
-            instance = super(WebsiteForm, self).save(commit=False)
-            instance.dropbox_user = self.dropbox_user
-            if commit:
-                instance.save()
-            return instance
+    def save(self, commit=True):
+        instance = super(WebsiteForm, self).save(commit=False)
+        instance.dropbox_user = self.dropbox_user
+        if commit:
+            instance.save()
+        return instance
