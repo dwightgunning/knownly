d9764a7a43ef5a6447c999e433a6a9bd154fe87c
Dwight Gunning <dwight@dwightgunning.com>
2015-11-06 14:01:15 +0100

    Configure raven logging handler for Sentry

--- knownly/settings_logging.py
+++ knownly/settings_logging.py
@@ -1,56 +1,74 @@
-# Django built-in loggers: django, django.request, django.db.backends, django.security.*
+# Django built-in loggers: django, django.request,
+#                          django.db.backends, django.security.*
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
     'formatters': {
         'verbose': {
-            'format': '[%(asctime)s %(levelname)s %(module)s %(process)d %(thread)d] %(message)s'
+            'format': '[%(asctime)s %(levelname)s %(module)s '
+                      '%(process)d %(thread)d] %(message)s'
         },
         'simple': {
             'format': '[%(asctime)s: %(levelname)s] %(message)s'
         },
     },
     'handlers': {
-        'console':{
-            'level':'DEBUG',
-            'class':'logging.StreamHandler',
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
             'formatter': 'verbose'
         },
-        'mail_admins': {
-            'level': 'WARNING',
-            'class': 'django.utils.log.AdminEmailHandler',
+        'sentry': {
+            'level': 'ERROR',
+            'class':
+                'raven.contrib.django.raven_compat.handlers.SentryHandler',
             'formatter': 'verbose'
-        },
+        }
     },
     'loggers': {
         'knownly.console.middleware': {
-            'handlers':['console', 'mail_admins'],
-            'level':'WARNING',
+            'handlers': ['console', 'sentry'],
+            'level': 'WARNING',
             'propagate': False,
         },
         'knownly': {
-            'handlers':['console', 'mail_admins'],
-            'level':'WARNING',
+            'handlers': ['console', 'sentry'],
+            'level': 'WARNING',
         },
         'django.db.backends': {
-            'handlers':['console', 'mail_admins'],
-            'level':'WARNING',
+            'handlers': ['console', 'sentry'],
+            'level': 'WARNING',
             'propagate': False,
         },
         'django.request': {
-            'handlers':['console', 'mail_admins'],
-            'level':'WARNING',
+            'handlers': ['console', 'sentry'],
+            'level': 'WARNING',
             'propagate': False,
         },
         'django': {
-            'handlers':['console', 'mail_admins'],
-            'level':'WARNING',
+            'handlers': ['console', 'sentry'],
+            'level': 'WARNING',
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'sentry'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console', 'sentry'],
+            'propagate': False,
         },
     },
 }
 
 from django.conf import settings
+
 if settings.DEBUG:
     # make all loggers use the console.
     for logger in LOGGING['loggers']:
