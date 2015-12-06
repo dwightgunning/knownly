21bffc32b1644c1785013e0236afbff3125741a5
Dwight Gunning <dwight@dwightgunning.com>
2015-12-06 23:54:54 +0100

    Fixed trailing slash issue on landing pages

--- knownly/landingpages/urls.py
+++ knownly/landingpages/urls.py
@@ -7,27 +7,27 @@ from knownly.plans import views as plans_views
 urlpatterns = \
     patterns('',
              # Landing pages
-             url(r'^welcome/developers$',
+             url(r'^welcome/developers/$',
                  TemplateView.as_view(
                      template_name="landingpages/developers.html"),
                  name='lp-developers'),
-             url(r'^welcome/designers$',
+             url(r'^welcome/designers/$',
                  TemplateView.as_view(
                      template_name="landingpages/designers.html"),
                  name='lp-designers'),
-             url(r'^welcome/learning-to-code$',
+             url(r'^welcome/learning-to-code/$',
                  TemplateView.as_view(
                      template_name="landingpages/learning-to-code.html"),
                  name='lp-learning-to-code'),
-             url(r'^welcome/creatives$',
+             url(r'^welcome/creatives/$',
                  TemplateView.as_view(
                      template_name="landingpages/creatives.html"),
                  name='lp-creatives'),
-             url(r'^welcome/founders$',
+             url(r'^welcome/founders/$',
                  TemplateView.as_view(
                      template_name="landingpages/founders.html"),
                  name='lp-founders'),
-             url(r'^welcome/startupweekend$',
+             url(r'^welcome/startupweekend/$',
                  TemplateView.as_view(
                      template_name="landingpages/startupweekend.html"),
                  name='lp-startupweekend'),
